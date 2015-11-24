b66b4f986339793375ceacf524bffcde9e932f81
Fangyang Cui <fc5fz@virginia.edu>
2015-11-23 19:56:07 -0500

    initial data collection

---
+++ dataCollection.py
@@ -0,0 +1,34 @@
+import blockcypher, requests, pickle, os
+
+#can be used to get data on old blocks
+
+def address_display(adr):
+    try:
+        r = requests.get('https://www.walletexplorer.com/api/1/address-lookup?address='
+		         + adr + '&caller=virginia.edu').json()
+        return adr + " (" + r['label'] + ")"
+    except:
+        return adr
+
+
+def satoshi_to_BTC(sval):
+    return sval / 100000000
+
+api_key= "a5eff945993d52cf8fce0cb1a7eda4a7"
+
+if os.path.exists("data.txt"):
+    file = open("data.txt", mode="rb")
+    blockData = pickle.load(file)
+    file.close()
+else:
+    blockData = {}
+
+#range of blocks to download
+for i in range(384000, 385000):
+    block = blockcypher.get_block_overview(i, api_key=api_key)
+    blockData[i] = block
+
+
+file = open("data.txt", mode = "wb")
+pickle.dump(blockData,file)
+file.close()
